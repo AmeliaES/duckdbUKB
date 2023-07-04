1c4b71967cded5bf1f81122118312d0bb188b543
Amelia Edmondson-Stait (AJES) <amelia.edmondson-stait@ed.ac.uk>
2023-07-04 13:33:11 +0100

    add comments

--- worked_example.R
+++ worked_example.R
@@ -22,8 +22,8 @@ library(tidyverse)
 # This is a weird bug I come across stometimes.
 
 # ------------------------------------
-# Read in the field IDs we want
-# (this will be updated to be an object loaded when you load in the package)
+# Read in the field IDs we want (below are some fieldIDs for variables that Alex wanted and is provided as an example. You could also make a variable with the field IDs that you are interested in.)
+# (this example data will eventually be cleaned and added as an object in the package)
 UKB_vars_df <- read.csv("/exports/igmm/eddie/GenScotDepression/amelia/duckdb_UKB/UKB_vars.csv")
 
 UKB_vars <- UKB_vars_df$Field.ID
@@ -34,6 +34,8 @@ UKB_vars <- UKB_vars[nzchar(UKB_vars)] %>% # removes empty strings
   sapply(., "[[", 1) %>%
   unique() %>%
   as.numeric()
+ 
+# The format of "UKB_vars" looks like this: "c(30710, 21003, 41270)", where each of these numbers is a field ID from UK Biobank (see the datashow case to search for your own: https://biobank.ndph.ox.ac.uk/showcase/)
 
 # ------------------------------------
 # Load in function we need (will fix this to install package instead when I can get it to work)
@@ -56,13 +58,14 @@ flatTable <- function(fieldIDs, instance = 0){
   # Load the data dictionary
   Dictionary <- tbl(con, 'Dictionary')
 
-  # Make a dataframe that tells you which table each field ID is in
+  # Make a dataframe that tells you which table each field ID is in. 
+  # ie. This is data frame with 2 columns "Table" and "FieldID", where Table is the table in the duckDB where you can find it's corresponding "FieldID".
   fields_df <- Dictionary |>
     filter(FieldID %in% fieldIDs) |>
     select(c(Table, FieldID)) |>
     collect()
 
-  # Create environment variables for each table, (assigned from it's character string name)
+  # Create global environment variables for each table, (assigned from it's character string name) so we can query them later
   tables <- unique(fields_df$Table)
   for(tab in tables){
     assign(tab, tbl(con, tab))
@@ -70,6 +73,7 @@ flatTable <- function(fieldIDs, instance = 0){
 
   # Create a list for each table with the field IDs selected
   # Reduce these and join by the ID column
+  # Either do this for the instance specified (first "if" code chunk) or for all instances ("else if" code chunk)
   if(instance %in% c(0:3, as.character(0:3))){
     flat_table_df <- lapply(tables, function(tab){
       tmp <- fields_df |>
@@ -96,17 +100,20 @@ flatTable <- function(fieldIDs, instance = 0){
 }
 
 # ------------------------------------
-# Try with just instance = 0
-ft <- flatTable(UKB_vars)
-head(ft)
+# Instance = 0 (default) - remember that an instance refers to an assessment time point.
+ft_0 <- flatTable(UKB_vars)
+head(ft_0)
 
-# Try all instances
+# Instance = 1
+ft_1 <- flatTable(UKB_vars, instance = 1)
+head(ft_1)
+
+# All instances:
 ft_all <- flatTable(UKB_vars, instance = "all")
-
 head(ft_all)
 
 # ------------------------------------
 # Save the table to your scratch space
-
 write.csv(ft_all, "UKB_flat.csv", quote = F, row.names = F)
 
+# Go back to the worked_example.md file to copy this across to data store.
